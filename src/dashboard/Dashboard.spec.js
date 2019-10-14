4d7b73bee6612123ff81e6c5e44dc678b820f4a3
Jnmendza <jonatmendoza@gmail.com>
2019-10-14 12:55:42 -0700

    Snapshots for each component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,11 @@
 // Test away
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import Dashboard from './Dashboard';
+
+test('Controls renders correctly', () => {
+    expect(render(<Dashboard />)).toMatchSnapshot();
+    render(<Dashboard />);
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,11 @@
 // Test away!
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import Display from './Display';
+
+test('Controls renders correctly', () => {
+    expect(render(<Display />)).toMatchSnapshot();
+    render(<Display />);
+});
